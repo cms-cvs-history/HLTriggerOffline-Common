5579b59494c6a92fe50abd0a50cc416fd57128c9
cmsbuild <>
2009-02-22 14:44:44 +0000

    This commit was manufactured by cvs2git to create tag 'HLTriggerOffline- Common-V00-00-20'.
    Sprout from master 2009-02-19 21:44:36 UTC Jeffrey Berryhill <sha1-962c16289a65616d7050fdaaf5fa06f1f8225240@cern.ch> ''
    Cherrypick from master 2009-02-22 14:44:43 UTC Nuno Viegas Guerreiro Leonardo <sha1-c2958d5bf0c252791bec9eb3947de5286c342841@cern.ch> 'temporarily remove 4vector client in 30x':
        python/FourVectorHLTriggerOfflineClient_cfi.py
        python/HLTValidationHarvest_cff.py
    Delete:
        python/HLTValidationQTExample_cfi.pyc

--- python/HLTValidationHarvest_cff.py
+++ python/HLTValidationHarvest_cff.py
@@ -3,16 +3,17 @@ from HLTriggerOffline.Muon.PostProcessor_cfi import *
 from HLTriggerOffline.Egamma.EgammaPostProcessor_cfi import *
 from HLTriggerOffline.Top.PostProcessor_cfi import *
 #from HLTriggerOffline.SUSYBSM.SUSYBSM_postProcessor_cff import *
-from HLTriggerOffline.Common.FourVectorHLTriggerOfflineClient_cfi import *
+#from HLTriggerOffline.Common.FourVectorHLTriggerOfflineClient_cfi import *
 from HLTriggerOffline.HeavyFlavor.heavyFlavorValidationHarvestingSequence_cff import *
 #from HLTriggerOffline.Common.PostProcessorExample_cfi import *
+
 hltpostvalidation = cms.Sequence( 
     HLTMuonPostVal
     +HLTTauPostVal
     +EgammaPostVal
     +HLTTopPostVal
    #+SusyExoPostVal
-    +hltFourVectorClient
+   #+HLTriggerOfflineFourVectorClient
     +heavyFlavorValidationHarvestingSequence
    #+ExamplePostVal
     )
